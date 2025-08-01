86eac2aaad81c5401cdb465787566e15a1ecf7d0
Emon Ahmed <bbzemonemon@gmail.com>
2025-08-01 09:53:21 +0600

    add new feature

--- script.js
+++ script.js
@@ -60,10 +60,40 @@ circles.forEach(elem => {
 
     const pointsMarked = elem.querySelectorAll('.points');
     for (let i = 0; i < percent; i++) {
-        pointsMarked[i].classList.add('marked'); // ✅ classlist → classList
+        pointsMarked[i].classList.add('marked'); 
     }
 });
 
 
 
+// active menu
+let menuLi = document.querySelectorAll('header ul li a');
+let section = document.querySelectorAll('section');
 
+function activeMenu(){
+    let len = section.length; 
+
+    while(--len && window.scrollY + 97 < section[len].offsetTop){}
+    menuLi.forEach(sec => sec.classList.remove("active"));
+    menuLi[len].classList.add("active");
+}
+
+activeMenu();
+window.addEventListener("scroll",activeMenu);
+
+
+
+
+let menuIcon = document.querySelector("#menu-icon");
+let navlist = document.querySelector(".navlist");
+
+
+menuIcon.onclick = ()=>{
+    menuIcon.classList.toggle("bx-x");
+    navlist.classList.toggle("open");
+}
+
+window.onscroll = ()=>{
+    menuIcon.classList.remove("bx-x");
+    navlist.classList.remove("open");
+}
